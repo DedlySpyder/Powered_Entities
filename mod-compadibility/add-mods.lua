dee6c503bb58ff2bb1208321d9b69a726796df00
DedlySpyder <dedlyspyder@gmail.com>
2018-03-24 12:38:47 -0400

    Added addtional mod compatibilities

--- mod-compadibility/add-mods.lua
+++ mod-compadibility/add-mods.lua
@@ -27,3 +27,190 @@ function check_FlareStack(mod)
 		})
 	end
 end
+
+function check_realisticreactors(mod)
+	if (mod == "RealisticReactors") then
+		add_to_table({
+			{"3x3", "rr-cooling-tower"},
+			{"3x3", "realistic-reactor-eccs"}
+		})
+	end
+end
+
+function check_airfilteringpatched(mod)
+	if (mod == "air-filtering-patched") then
+		add_to_table({
+			{"3x3", "air-filter-machine-mk1"},
+			{"3x3", "air-filter-machine-mk2"},
+			{"3x3", "air-filter-machine-mk3"},
+			{"3x3", "air-filter-machine-mk4"},
+			{"3x3", "air-filter-machine-mk5"}
+		})
+	end
+end
+
+function check_bioindustries(mod)
+	if (mod == "Bio_Industries") then
+		add_to_table({
+			{"2x2", "bi-stone-crusher"},
+			{"3x3", "bio-greenhouse"},
+			{"3x3", "bio_boiler"},
+			{"3x3", "bi-cokery"},
+			{"3x3", "bi-Bio_Garden"},
+			{"3x3", "bi-bioreactor"},
+			{"9x10", "bi-bio_farm"},
+			{"9x10", "bi_bio_Solar_Farm"},
+			{"5x5", "bi_accumulator"}
+		})
+	end
+end
+
+function check_darkmatterreplicators(mod)
+	if (mod == "dark-matter-replicators") then
+		add_to_table({
+			{"3x3", "replication-lab"},
+			{"2x2", "replicator-1"},
+			{"2x2", "replicator-2"},
+			{"2x2", "replicator-3"},
+			{"2x2", "replicator-4"},
+			{"2x2", "replicator-5"}
+		})
+	end
+end
+
+function check_deadlockcrating(mod)
+	if (mod == "DeadlockCrating") then
+		add_to_table({
+			{"3x3", "deadlock-machine-packer-entity-1"},
+			{"3x3", "deadlock-machine-packer-entity-2"},
+			{"3x3", "deadlock-machine-packer-entity-3"}
+		})
+	end
+end
+
+function check_electricfurnaces(mod)
+	if (mod == "Electric Furnaces") then
+		add_to_table({
+			{"2x2", "electric-stone-furnace"},
+			{"2x2", "electric-steel-furnace"},
+			{"3x3", "electric-furnace-2"},
+			{"3x3", "electric-furnace-3"},
+			{"3x3", "electric-boiler"}
+		})
+	end
+end
+
+function check_electrictrain(mod)
+	if (mod == "ElectricTrain") then
+		add_to_table({
+			{"2x2", "power-provider"}
+		})
+	end
+end
+
+function check_geothermal(mod)
+	if (mod == "Geothermal") then
+		add_to_table({
+			{"3x3", "geothermal-well"},
+			{"3x3", "geothermal-heat-exchanger"}
+		})
+	end
+end
+
+function check_kspower(mod)
+	if (mod == "KS_Power") then
+		add_to_table({
+			{"2x2", "wind-turbine-2"},
+			{"5x5", "petroleum-generator"},
+			{"3x3", "OilSteamBoiler"},
+			{"3x3", "burner-generator"}
+		})
+	end
+end
+
+function check_largelamp(mod)
+	if (mod == "LargeLamp") then
+		add_to_table({
+			{"1x1", "LargeLamp"}
+		})
+	end
+end
+
+function check_liquidscience(mod)
+	if (mod == "liquid-science") then
+		add_to_table({
+			{"3x3", "science-pump"}
+		})
+	end
+end
+
+function check_miningtools(mod)
+	if (mod == "MiningTools") then
+		add_to_table({
+			{"3x3", "fast-mining-drill"},
+			{"3x3", "water-injector"}
+		})
+	end
+end
+
+function check_plc(mod)
+	if (mod == "plc") then
+		add_to_table({
+			{"3x3", "plc-unit"}
+		})
+	end
+end
+
+function check_railloader(mod)
+	if (mod == "railloader") then
+		add_to_table({
+			{"4x4", "railunloader-chest"},
+			{"4x4", "railloader-chest"}
+		})
+	end
+end
+
+function check_scanningradar(mod)
+	if (mod == "ScanningRadar") then
+		add_to_table({
+			{"3x3", "scanning-radar"}
+		})
+	end
+end
+
+function check_vehicleradar(mod)
+	if (mod == "Vehicle_Radar") then
+		add_to_table({
+			{"3x3", "vehicular-tracker"},
+			{"3x3", "train-tracker"}
+		})
+	end
+end
+
+function check_waterwell(mod)
+	if (mod == "WaterWell") then
+		add_to_table({
+			{"3x3", "water-well-pump"}
+		})
+	end
+end
+
+function check_wirelesssignals(mod)
+	if (mod == "wireless-signals") then
+		add_to_table({
+			{"2x2", "ws-radio-transmitter-1"},
+			{"2x2", "ws-radio-transmitter-2"},
+			{"2x2", "ws-radio-repeater"},
+			{"2x2", "ws-radio-receiver"}
+
+		})
+	end
+end
+
+function check_stormwalls(mod)
+	if (mod == "Stormwalls") then
+		add_to_table({
+			{"5x5", "stormwall-emitter"}
+		})
+	end
+end
